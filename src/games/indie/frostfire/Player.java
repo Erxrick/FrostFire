40991274586ec18cadc25c40c711969d48dae582
Zachery Anderson <zanderson@student.neumont.edu>
2017-02-11 16:12:59 -0700

    Players can now move in 8 directions off of WASD alone. Still need controller support to be built.

--- src/games/indie/frostfire/Player.java
+++ src/games/indie/frostfire/Player.java
@@ -11,22 +11,32 @@ public class Player extends Human {
 		
 	}
 
-	public void updatePosition(GameContainer gc) {
+	public void updatePosition(GameContainer gc) {	
+		float speed = 1.6f;
 		if (gc.getInput().isKeyDown(Input.KEY_W)) {
-			move(90, 1);
+			if(gc.getInput().isKeyDown(Input.KEY_W) && gc.getInput().isKeyDown(Input.KEY_A)) {
+				move((180-45),speed);
+			} else if(gc.getInput().isKeyDown(Input.KEY_W) && gc.getInput().isKeyDown(Input.KEY_D)) {
+				move((45),speed);
+			} else {
+				move(90, speed);
+			}
 		}
-		if (gc.getInput().isKeyDown(Input.KEY_A)) {
-			move(180, 1);
+		else if (gc.getInput().isKeyDown(Input.KEY_S)) {
+			if(gc.getInput().isKeyDown(Input.KEY_A) && gc.getInput().isKeyDown(Input.KEY_S)) {
+				move((270-45),speed);
+			} else if(gc.getInput().isKeyDown(Input.KEY_D) && gc.getInput().isKeyDown(Input.KEY_S)) {
+				move((360-45),speed);
+			} else {
+				move(270, speed);
+			}
 		}
-		if (gc.getInput().isKeyDown(Input.KEY_S)) {
-			move(270, 1);
+		else if (gc.getInput().isKeyDown(Input.KEY_A)) {
+			move(180, speed);
 		}
-		if (gc.getInput().isKeyDown(Input.KEY_D)) {
-			move(0, 1);
-		}
-		
-		
-			
+		else if (gc.getInput().isKeyDown(Input.KEY_D)) {
+			move(0, speed);
+		}			
 	}
 	
 	
